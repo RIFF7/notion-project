5de1e9d5df10c458de8b724e2fe5412fbbad1471
RIFF7 <syarifudinjaelani@gmail.com>
2023-09-10 17:50:38 +0700

    Modifikasi belum selesai

--- game_2048/game_2048.py
+++ game_2048/game_2048.py
@@ -1,43 +1,78 @@
+# 2048.py
+
+# importing the logic_game.py file
+# where we have written all the
+# logic_game functions used.
 import logic_game
 
+# Driver code
 if __name__ == '__main__':
-    mat = logic_game.start_game()
+	
+# calling start_game function
+# to initialize the matrix
+	mat = logic_game.start_game()
 
 while(True):
-    x = input("Silakan Masukkan Arah: ")
-    if (x == 'W' | x == 'w'):
-        mat, flag = logic_game.move_up(mat)
-        status = logic_game.get_current_state(mat)
-        print(status)
-        
-        if (status == 'Game Not Over'):
-            logic_game.add_new_2(mat)
-        else:
-            break
-    elif (x == 'A' | x == 'a'):
-        mat, flag = logic_game.move_left(mat)
-        status = logic_game.get_current_state(mat)
-        print(status)
-        if (status == 'Game Not Over'):
-            logic_game.add_new_2(mat)
-        else:
-            break
-    elif (x == 'S' | x == 's'):
-        mat, flag = logic_game.move_down(mat)
-        status = logic_game.get_current_state(mat)
-        print(status)
-        if (status == 'Game Not Over'):
-            logic_game.add_new_2(mat)
-        else:
-            break
-    elif (x == 'D' | x == 'd'):
-        mat, flag = logic_game.move_right(mat)
-        status = logic_game.get_current_state(mat)
-        print(status)
-        if (status == 'Game Not Over'):
-            logic_game.add_new_2(mat)
-        else:
-            break
-    else:
-        print("Arah yang kamu masukkan tidak sesuai!!!")
-    print(mat)
+
+	# taking the user input
+	# for next step
+	x = input("Press the command : ")
+
+	# we have to move up
+	if(x == 'W' or x == 'w'):
+
+		# call the move_up function
+		mat, flag = logic_game.move_up(mat)
+
+		# get the current state and print it
+		status = logic_game.get_current_state(mat)
+		print(status)
+
+		# if game not over then continue
+		# and add a new two
+		if(status == 'GAME NOT OVER'):
+			logic_game.add_new_2(mat)
+
+		# else break the loop
+		else:
+			break
+
+	# the above process will be followed
+	# in case of each type of move
+	# below
+
+	# to move down
+	elif(x == 'S' or x == 's'):
+		mat, flag = logic_game.move_down(mat)
+		status = logic_game.get_current_state(mat)
+		print(status)
+		if(status == 'GAME NOT OVER'):
+			logic_game.add_new_2(mat)
+		else:
+			break
+
+	# to move left
+	elif(x == 'A' or x == 'a'):
+		mat, flag = logic_game.move_left(mat)
+		status = logic_game.get_current_state(mat)
+		print(status)
+		if(status == 'GAME NOT OVER'):
+			logic_game.add_new_2(mat)
+		else:
+			break
+
+	# to move right
+	elif(x == 'D' or x == 'd'):
+		mat, flag = logic_game.move_right(mat)
+		status = logic_game.get_current_state(mat)
+		print(status)
+		if(status == 'GAME NOT OVER'):
+			logic_game.add_new_2(mat)
+		else:
+			break
+	else:
+		print("Invalid Key Pressed")
+
+	# print the matrix after each
+	# move.
+	print(mat)
