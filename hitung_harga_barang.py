e15f0cdfbdb442f9bdc6d37a83ac0990f7ee2ac1
RIFF7 <syarifudinjaelani@gmail.com>
2023-10-13 17:05:46 +0700

    Membuat file baru untuk melakukan perhitungan sederhana dengan perpaduan dictionary sebagai penyimpanan data

---
+++ hitung_harga_barang.py
@@ -0,0 +1,52 @@
+"""
+    Membuat perhitungan sederhana untuk pembelian bawang pada list dibawah ini:
+    
+    -------------------------------------------------------------
+    |   Nama Barang     |       Harga       |       Diskon      |
+    -------------------------------------------------------------
+    |   Sepatu Nike     |     2300000       |       175000      |
+    |   Baju Uniklo     |     127000        |       23500       |
+    |   Celanan Jeans   |     175000        |       15000       |
+    |   Jacket Bomber   |     235000        |         0         |
+    |   Kaca Mata Hitam |     112000        |       17000       |
+    -------------------------------------------------------------
+    
+    Dari data belanja yang kita punya diatas, kita akan membuat program
+    sederhana dimana untuk menghitung total belanja yang sudah dihitung juga
+    dengan total pajak sebanyak 10%, sehingga kita mendapat total bersih
+    dari banyaknya uang yang kita belanjakan untuk 5 item diatas 😂
+"""
+
+# Hal pertama yang dilakukan adalah membuat dictionary yang akan
+# menampung data belanjaan kita
+
+sepatu = {
+    "nama": "Sepatu Nike",
+    "harga": 2300000,
+    "diskon": 175000
+    }
+
+baju = {
+    "nama": "Baju Uniklo",
+    "harga": 127000,
+    "diskon": 23500
+}
+
+celana = {
+    "nama": "Celanan Jeans",
+    "harga": 175000,
+    "diskon": 23500
+}
+
+jaket = {
+    "nama": "Jacket Bomber",
+    "harga": 235000,
+    "diskon": 0
+}
+
+aksesoris = {
+    "nama": "Kaca Mata Hitam",
+    "harga": 112000,
+    "diskon": 17000
+}
+
