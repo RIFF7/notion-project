e02c1914f2cb630851486d085d57feab84224ddd
RIFF7 <syarifudinjaelani@gmail.com>
2023-10-15 17:10:10 +0700

    Menambahkan code baru untuk menghitung total tagihan dalam project pembuatan warehousing

---
+++ perhitungan_data_warehouse.py
@@ -0,0 +1,20 @@
+"""
+    Membuat perhitungan pembayaran sederhana untuk jasa 
+    pembuatan data warehouse, dengan data dibawah ini:
+    
+    -------------------------------------------------------------------------------------------------------
+    |       Nama Jasa       |       Harga per-Hari       |       Total Hari      |        Sub Total       |
+    -------------------------------------------------------------------------------------------------------
+    |   Data Warehousing    |         1000000           |           15          |         15000000        |
+    |   Dat Cleanning       |         1500000           |           10          |         15000000        |
+    |   Data Integration    |         2000000           |           15          |         30000000        |
+    |   Data Trabsformation |         2500000           |           10          |         25000000        |
+    ------------------------------------------------------------------------------------------------------|
+    |   Total               |                                                   |         85000000        |
+    ------------------------------------------------------------------------------------------------------|
+    
+    Dari data yang telah kita miliki diatas saat ini tujuannya adalah,
+    menghitung tagihan pembayaran dimana isi dari table diatas
+    akan kita masukkan pada dictionary yang nantinya akan dilakukan
+    sebuah perhitungan untuk mengetahui besaran tagihan yang harus dibayarkan
+"""
